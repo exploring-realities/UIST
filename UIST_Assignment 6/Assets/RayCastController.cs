6f8fe2de97ecea6fe7a59c3aeac44d3cb0f4d439
pandafork <frau.gabel@gmail.com>
2017-11-30 23:09:11 +0100

    - UIST assignment 6 - part 1 working, part 2 (raycasting) needs to be fixed

---
+++ UIST_Assignment 6/Assets/RayCastController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RayCastController : MonoBehaviour {
+
+
+	public Material highlightMat;
+	public GameObject camera;
+	private Renderer objRend;
+	private Material initialMat;
+	private GameObject selectedObj;
+	private bool selected = false;
+
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+			RaycastHit hit;
+		Vector3 fwd = transform.TransformDirection(Vector3.back);
+		Debug.DrawRay(transform.position, fwd, Color.red);
+
+
+		objRend.material = initialMat;
+		
+
+        if (Physics.Raycast(transform.position, fwd, out hit)){
+		
+			if(!selected){
+
+				selectedObj = hit.collider.transform.gameObject;
+				objRend = selectedObj.GetComponent<Renderer>();
+				initialMat = objRend.material;
+
+				selected = true;
+				objRend.material = highlightMat;
+
+			}
+
+		}
+
+
+	}
+}
---
+++ UIST_Assignment 6/Assets/VirtualHandController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class VirtualHandController : MonoBehaviour {
+
+public Material highlightMat;
+public GameObject camera;
+private Renderer objRend;
+private Material initialMat;
+private GameObject selectedObj;
+private bool selected = false;
+
+
+
+
+	void Update(){
+
+	
+            
+		//handling grabbing and releasing of object when space is pressed/released
+		if(Input.GetKeyDown(KeyCode.Space) && selected){
+			
+			 selectedObj.transform.parent = this.transform;
+			
+		} else if(Input.GetKeyUp(KeyCode.Space)){
+
+			selectedObj.transform.parent = camera.transform;
+		}
+
+
+	}
+
+
+	public void OnTriggerEnter(Collider collider) {
+		
+		selectedObj = collider.transform.gameObject;
+		objRend = selectedObj.GetComponent<Renderer>();
+		initialMat = objRend.material;
+
+		selected = true;
+		objRend.material = highlightMat;
+
+	
+
+	}
+
+
+	public void OnTriggerExit(Collider collider)
+	{
+		selected = false;
+		objRend.material = initialMat;
+		
+	}
+
+
+
+
+
+}
